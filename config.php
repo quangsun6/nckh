c5f99c29d9479a262233488280d9e225f6bffb9d
gnuhpdiem <159976941+gnuhpdiem@users.noreply.github.com>
2024-05-13 09:39:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,15 @@
 <?php 
 
-    $conn = new mysqli("localhost","root","","my_db");
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "my_db";
+
+    $conn = new mysqli($server, $username, $password, $dbname);
 
     // Check connection
-    if ($conn -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $conn -> connect_error;
-        exit();
-    }
+    if ($conn->connect_error) {
+        die("connect failed!<br>Reason: " . $conn->connect_error);
+    } // else echo "connected!";
 
 ?>
